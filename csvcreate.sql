76194c7c7d6269f8b454f7eb3937acfa0eb35996
Paul2004-sza <sutzawaungnhkum@gmail.com>
2025-03-05 12:22:10 +0700

    to date

--- csvcreate.sql
+++ csvcreate.sql
@@ -1,34 +1,25 @@
-LOAD DATA INFILE 'users.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/users.csv'
 INTO TABLE Users
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'bookings.csv'
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/bookings.csv'
 INTO TABLE Bookings
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'campaigns.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/campaigns.csv'
 INTO TABLE Marketing_Campaigns
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'campaign_performance.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/campaign_performance.csv'
 INTO TABLE Campaign_Performance
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
-LOAD DATA INFILE 'reviews.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/reviews.csv'
 INTO TABLE Reviews
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
-
-SELECT 
-    c.channel,
-    SUM(p.revenue_generated) AS total_revenue,
-    SUM(p.conversions) AS total_conversions,
-    SUM(p.clicks) AS total_clicks
-FROM Campaign_Performance p
-JOIN Marketing_Campaigns c ON p.campaign_id = c.campaign_id
-GROUP BY c.channel
-ORDER BY total_revenue DESC;
